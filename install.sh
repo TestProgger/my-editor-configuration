90bc177a702337b94aeb3f7969e477edb9d92f9b
TestProgger <36998526+TestProgger@users.noreply.github.com>
2021-08-27 13:28:02 +0300

    Updated install.sh : Added Oh-My-Zsh installation Scripts

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ cp "$REP_PATH/.tmux.conf" ~/.tmux.conf
 cp -rf "$REP_PATH/nvim" ~/.config/
 
 # Installing zsh plugins
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
